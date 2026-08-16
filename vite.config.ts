f426ffae3cab23d76b048bb20aa178b8a8ebe229
pf <xiaofei450@126.com>
2026-08-16 11:29:11 +0800

    配置 GitHub Pages 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ai_welding/',
   plugins: [react()],
   resolve: {
     alias: {
